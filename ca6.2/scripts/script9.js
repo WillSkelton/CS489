1b782ff18a06a8622ea64ee3249515041933e6f4
Will Skelton <william.f.skelton@gmail.com>
2021-10-01 14:14:07 -0700

    Done with task 1

--- ca6.2/scripts/script9.js
+++ ca6.2/scripts/script9.js
@@ -1,9 +1,15 @@
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable react/jsx-filename-extension */
 class Clock extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = { date: new Date() };
+		this.state = {
+			date: new Date(),
+			showTime: true,
+		};
 	}
 
+	// minimized because it didn't change
 	componentDidMount() {
 		this.timer = setInterval(
 			() => this.updateTime(),
@@ -11,6 +17,7 @@ class Clock extends React.Component {
 		);
 	}
 
+	// minimized because it didn't change
 	componentWillUnmount() {
 		clearInterval(this.timer);
 	}
@@ -22,14 +29,20 @@ class Clock extends React.Component {
 	}
 
 	render() {
+		const { showTime, date } = this.state;
 		return (
 			<div>
 				<h1>Hello React Students!</h1>
 				<h2>
-					It is
-					{this.state.date.toLocaleTimeString()}
-					.
+					{
+						showTime && `It is ${date.toLocaleTimeString()}.`
+					}
 				</h2>
+				<button type="button" onClick={() => this.setState({ showTime: !showTime })}>
+					{
+						showTime ? 'Hide Time' : 'Show Time'
+					}
+				</button>
 			</div>
 		);
 	}
