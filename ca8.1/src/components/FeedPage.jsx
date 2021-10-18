4304c05d6fd87b7a426ee0d4b63095cd3a43d2c7
Will Skelton <william.f.skelton@gmail.com>
2021-10-18 15:30:41 -0700

    done with ca8.1

---
+++ ca8.1/src/components/FeedPage.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+
+const style = {
+	position: "fixed",
+	display: "flex",
+	flexDirection: "column",
+	alignItems: "center",
+	justifyContent: "center",
+	height: "100%",
+	width: "100%",
+}
+
+export function FeedPage() {
+	return (
+		<div style={style}>
+
+			<h1>
+				The Feed Page is Under Construction
+			</h1>
+		</div>
+	)
+}
